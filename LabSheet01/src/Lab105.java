793370288c3933c76ba451c1a8d1979336e98f9e
w-joe-verojna <2413130010@tni.ac.th>
2025-11-17 17:35:24 +0700

    commit Lab105

---
+++ LabSheet01/src/Lab105.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Lab105 {
+    static void main() {
+        JOptionPane.showMessageDialog(null,"Summation number = " + (2 + 5) +
+                "\nSubtraction number = " + (2 - 5) +
+                "\nMultiplication number = " + (2 * 5) +
+                "\nDivision number = " + (2 / 5) +
+                "\nModulus number = " + (2 % 5),
+                "Arithmatic Operation",
+                JOptionPane.WARNING_MESSAGE);
+    }
+}
