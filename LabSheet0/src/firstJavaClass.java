50c5e36ea717f82d76899c84d8f4d3c32c969a35
w-joe-verojna <2413130010@tni.ac.th>
2025-11-25 14:52:42 +0700

    LabSheet0

---
+++ LabSheet0/src/firstJavaClass.java
@@ -0,0 +1,5 @@
+public class firstJavaClass {
+    static void main() {
+        System.out.println("Hello, World");
+    }
+}
---
+++ LabSheet0/src/secondJavaClass.java
@@ -0,0 +1,5 @@
+public class secondJavaClass {
+    static void main() {
+        System.out.println("Hello, Java");
+    }
+}
